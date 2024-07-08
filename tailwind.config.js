82ff13eff6860b58814045d53f9059bd716adc20
rokas <rokas@rokas.com>
2024-07-08 09:43:23 +0300

    Add new files for VCS settings, post data, and components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./navigation/**/*.{js,ts,jsx,tsx}",
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {},
